503e77eaebd1afd35d6cfd530f5478d1eca2110b
DimuthuShasantha <mdimuthusw@gmail.com>
2023-12-17 17:25:15 +0530

    create navbar and routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,20 @@
 import { Routes, Route } from "react-router-dom";
+import Signin from "./pages/Signin";
+import Signup from "./pages/Signup";
+import Dashboard from "./pages/Dashboard";
+import NavBar from "./components/NavBar";
 
-export default function App() {
+const App = () => {
   return (
-    <Routes>
-      <Route />
-    </Routes>
-  )
-}
+    <>
+      <NavBar />
+      <Routes>
+        <Route path="/signin" element={<Signin />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/" element={<Dashboard />} />
+      </Routes>
+    </>
+  );
+};
+
+export default App;
---
+++ client/src/components/NavBar.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const NavBar = () => {
+  return (
+    <div className="shadow-sm bg-slate-200">
+      <div className="flex items-start justify-between max-w-6xl p-3 mx-auto">
+        <Link to="/" className="text-lg font-bold text-slate-800">mernCrud</Link>
+        <div className="flex gap-3">
+          <Link to="/" className="cursor-pointer hover:opacity-95 text-slate-800">Home</Link>
+          <Link to="/about" className="cursor-pointer hover:opacity-95 text-slate-800">About</Link>
+          <Link to="/signup" className="cursor-pointer hover:opacity-95 text-slate-800">Sign Up</Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default NavBar;
